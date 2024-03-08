6c470d7ec2d366eb03234b1184495f86b7fc825d
SWEET-TOXIC-DEVIL <153902241+SWEET-TOXIC-DEVIL@users.noreply.github.com>
2024-03-08 10:39:55 +0530

    🎵🎶ꜱᴡᴇᴇᴛ_ᴛᴏxɪᴄ_ᴍᴜꜱɪᴄ🩸🖤🫀

--- SWEET_TOXIC_MUSIC/platforms/Soundcloud.py
+++ SWEET_TOXIC_MUSIC/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from EsproMusic.utils.formatters import seconds_to_min
+from SWEET_TOXIC_MUSIC.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
