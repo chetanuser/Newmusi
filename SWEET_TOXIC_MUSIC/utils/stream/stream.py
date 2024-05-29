d7be6ceb787f523721b0df1e60018e020d26ecf2
WEREWOLF-DEMON <153902241+WEREWOLF-DEMON@users.noreply.github.com>
2024-05-29 23:59:25 +0530

    🎵🎶ꜱᴡᴇᴇᴛ_ᴛᴏxɪᴄ_ᴍᴜꜱɪᴄ🩸🖤🫀

--- SWEET_TOXIC_MUSIC/utils/stream/stream.py
+++ SWEET_TOXIC_MUSIC/utils/stream/stream.py
@@ -6,12 +6,12 @@ from pyrogram.types import InlineKeyboardMarkup
 
 import config
 from SWEET_TOXIC_MUSIC import Carbon, YouTube, app
-from SWEET_TOXIC_MUSIC.core.call import Ritik
+from SWEET_TOXIC_MUSIC.core.call import WEREWOLF_DEMON
 from SWEET_TOXIC_MUSIC.misc import db
 from SWEET_TOXIC_MUSIC.utils.database import add_active_video_chat, is_active_chat
 from SWEET_TOXIC_MUSIC.utils.exceptions import AssistantErr
 from SWEET_TOXIC_MUSIC.utils.inline import aq_markup, close_markup, stream_markup
-from SWEET_TOXIC_MUSIC.utils.pastebin import RitikBin
+from SWEET_TOXIC_MUSIC.utils.pastebin import WEREWOLF_DEMONBin
 from SWEET_TOXIC_MUSIC.utils.stream.queue import put_queue, put_queue_index
 from SWEET_TOXIC_MUSIC.utils.thumbnails import get_thumb
 
@@ -32,7 +32,7 @@ async def stream(
     if not result:
         return
     if forceplay:
-        await Ritik.force_stop_stream(chat_id)
+        await WEREWOLF_DEMON.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['play_19']}\n\n"
         count = 0
@@ -79,7 +79,7 @@ async def stream(
                     )
                 except:
                     raise AssistantErr(_["play_14"])
-                await Ritik.join_call(
+                await WEREWOLF_DEMON.join_call(
                     chat_id,
                     original_chat_id,
                     file_path,
@@ -116,7 +116,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await RitikBin(msg)
+            link = await WEREWOLF_DEMONBin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -165,7 +165,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Ritik.join_call(
+            await WEREWOLF_DEMON.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -225,7 +225,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Ritik.join_call(chat_id, original_chat_id, file_path, video=None)
+            await WEREWOLF_DEMON.join_call(chat_id, original_chat_id, file_path, video=None)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -277,7 +277,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Ritik.join_call(chat_id, original_chat_id, file_path, video=status)
+            await WEREWOLF_DEMON.join_call(chat_id, original_chat_id, file_path, video=status)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -333,7 +333,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await Ritik.join_call(
+            await WEREWOLF_DEMON.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -391,7 +391,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Ritik.join_call(
+            await WEREWOLF_DEMON.join_call(
                 chat_id,
                 original_chat_id,
                 link,
